137d6a154b5dea4db459f08820787a5a8ab4acff
FabBarbosa <91352941+FabBarbosa@users.noreply.github.com>
2023-11-23 18:43:37 -0300

    Deploy

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -7,8 +7,10 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class ApiApplication {
 
+	public String PORT = System.getenv("PORT");
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+
 	}
 
 }
